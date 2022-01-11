bd1a01a6ac1bd935da88ecc660e1c19d38a49a38
pranta-barua007 <prantabarua247@gmail.com>
2022-01-11 21:03:40 +0600

    converting directory page from using Redux to Apollo Client 3

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import DirectoryContainer from '../../components/directory/directory.container';
-import Directory from '../../components/directory/directory.component';
 
 import './homepage.styles.scss';
 
