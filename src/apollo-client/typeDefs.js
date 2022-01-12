f75b00facee26ac8e01d0af9950f2834b82b4c74
pranta-barua007 <prantabarua247@gmail.com>
2022-01-12 12:59:48 +0600

    converted all of our application from using redux to apollo-clien-3

--- src/apollo-client/typeDefs.js
+++ src/apollo-client/typeDefs.js
@@ -4,4 +4,16 @@ export const typeDefs = gql`
     extend type Item {
         quantity: Int
     }
+
+    extend type DateTime {
+        nanoseconds: Int!
+        seconds: Int!
+    }
+    
+    extend type User {
+        id: ID!
+        displayName: String!
+        email: String!
+        createdAt: DateTime!
+    }
 `;
