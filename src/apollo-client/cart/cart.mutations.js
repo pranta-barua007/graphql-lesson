8ad420906527f9a1edbddb42ab77c79a471ea918
pranta-barua007 <prantabarua247@gmail.com>
2022-01-12 11:08:46 +0600

    introduced updateCart func to handle common functionality

--- src/apollo-client/cart/cart.mutations.js
+++ src/apollo-client/cart/cart.mutations.js
@@ -4,35 +4,35 @@ import { addItem, removeItem, clearItem, getCartItemCount, getCartItemsTotalCoun
 export const toggleCartHidden = () => {
     const hidden = cartHiddenVar(); //getting the value
     cartHiddenVar(!hidden); //setting the value
+    return !hidden;
 };
+
+const updateCart = (newCartItems) => {
+    const cartItemCount = getCartItemCount(newCartItems);
+    const cartItemsTotal = getCartItemsTotalCount(newCartItems);
+    cartItemsVar(newCartItems);
+    cartItemCountVar(cartItemCount);
+    cartItemsTotalVar(cartItemsTotal);
+}
 
 export const addItemToCart = (item) => {
     const cartItems  = cartItemsVar();
     const newCartItems = addItem(cartItems, item);
-    const cartItemCount = getCartItemCount(newCartItems);
-    const cartItemsTotal = getCartItemsTotalCount(newCartItems);
-    cartItemCountVar(cartItemCount);
-    cartItemsVar(newCartItems);
-    cartItemsTotalVar(cartItemsTotal);
+    updateCart(newCartItems);
+    return newCartItems;
 };
 
 export const removeItemFromCart = (item) => {
     const cartItems  = cartItemsVar();
     const newCartItems = removeItem(cartItems, item);
-    const cartItemCount = getCartItemCount(newCartItems);
-    const cartItemsTotal = getCartItemsTotalCount(newCartItems);
-    cartItemCountVar(cartItemCount);
-    cartItemsVar(newCartItems);
-    cartItemsTotalVar(cartItemsTotal);
+    updateCart(newCartItems);
+    return newCartItems;
 }
 
 export const clearItemFromCart = (item) => {
     const cartItems  = cartItemsVar();
     const newCartItems = clearItem(cartItems, item);
-    const cartItemCount = getCartItemCount(newCartItems);
-    const cartItemsTotal = getCartItemsTotalCount(newCartItems);
-    cartItemCountVar(cartItemCount);
-    cartItemsVar(newCartItems);
-    cartItemsTotalVar(cartItemsTotal);
+    updateCart(newCartItems);
+    return newCartItems;
 }
 
