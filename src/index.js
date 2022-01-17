37d692d189ae8ff08d0c401368cf0ee2968e71a6
pranta-barua007 <prantabarua247@gmail.com>
2022-01-17 23:13:17 +0600

    removed redux

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 
 import { ApolloProvider } from "@apollo/client";
 import { apolloClient } from "./apollo-client/client";
 
-import { store, persistor } from "./redux/store";
-
 import "./index.css";
-import { default as App } from './App/App.container';
+import { default as App } from "./App/App.container";
 
 ReactDOM.render(
   <ApolloProvider client={apolloClient}>
-    <Provider store={store}>
-      <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ApolloProvider>,
   document.getElementById("root")
 );
